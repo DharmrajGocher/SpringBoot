abae50e53c6f43af2c6dfcb68a2a8afee6d2d1a7
Dharmraj gocher <58349648+DharmrajGocher@users.noreply.github.com>
2019-11-30 10:32:59 +0530

    maven project sample

---
+++ SampleProjectMvn/src/main/java/com/Dharma/SampleProjectMvn/SampleProjectMvnApplication.java
@@ -0,0 +1,13 @@
+package com.Dharma.SampleProjectMvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectMvnApplication.class, args);
+	}
+
+}
---
+++ SampleProjectMvn/src/test/java/com/Dharma/SampleProjectMvn/SampleProjectMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dharma.SampleProjectMvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
