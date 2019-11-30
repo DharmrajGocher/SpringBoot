42a0ee86cf461269d8414c8a95a1bc2941162c1b
Dharmraj gocher <58349648+DharmrajGocher@users.noreply.github.com>
2019-11-30 08:06:20 +0530

    Addiing basic gradle spring boot application
    Addiing basic gradle spring boot application

---
+++ SampleApp/src/main/java/com/dharma/SampleApp/SampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.dharma.SampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppApplication.class, args);
+	}
+
+}
---
+++ SampleApp/src/test/java/com/dharma/SampleApp/SampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dharma.SampleApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
